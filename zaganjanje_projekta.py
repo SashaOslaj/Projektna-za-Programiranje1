25354d87387168870755569ef7638c79cb25be1d
SashaOslaj <sasa.oslaj@student.fmf.uni-lj.si>
2017-12-27 14:32:14 +0100

    Add files via upload

--- zaganjanje_projekta.py
+++ zaganjanje_projekta.py
@@ -1,4 +1,15 @@
 from Projekt import *
 
-result = get_url(stadiums_url)
-result.encoding
+#download = download_url_to_string(stadiums_url)
+
+#text = save_string_to_file(download, stadiums_directory, frontpage_filename)
+
+#result = get_url(stadiums_url)
+#result.encoding
+
+#reade_page = read_file_to_string(stadiums_directory, frontpage_filename)
+
+
+page = page_to_table(read_file_to_string(stadiums_directory, frontpage_filename))
+
+
