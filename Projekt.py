5d947dcec7c7e626a12c227d5e0937960762033f
SashaOslaj <sasa.oslaj@student.fmf.uni-lj.si>
2017-12-27 17:19:04 +0100

    Add files via upload

--- Projekt.py
+++ Projekt.py
@@ -1,11 +1,12 @@
 import requests
 import re
 import os
-import csv
+#import csv
 
 stadiums_url = 'https://en.wikipedia.org/wiki/List_of_stadiums_by_capacity'
 stadiums_directory = 'stadiums'
 frontpage_filename = 'stadiums.html'
+
 
 def download_url_to_string(url):
     try:
@@ -15,6 +16,7 @@ def download_url_to_string(url):
         return None
     return r.text
 
+
 def get_url(url):
     try:
         r = requests.get(url)
@@ -23,18 +25,21 @@ def get_url(url):
         return None
     return r
 
+
 def save_string_to_file(text, directory, filename):
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
-    
+
     with open(path, 'w', encoding='utf-8') as f:
         f.write(text)
     return None
+
 
 def save_url_to_a_file():
     text = download_url_to_string(stadiums_url)
     save_string_to_file(text, stadiums_directory, frontpage_filename)
     return None
+
 
 def read_file_to_string(directory, filename):
     path = os.path.join(directory, filename)
@@ -42,43 +47,23 @@ def read_file_to_string(directory, filename):
         return file_in.read()
 
 
-zapomni = (r'<tr>'
-            + r'(.*?)'
-            + r'<td><a href="(.*?)" title="(?P<title_of_stadium>.+?)">(.+?)</a></td>'
-            + r'(.*?)'
-            + r'<td>(?P<capacitet_of_stadium>.+?)<sup (.*?)></td>'
-            + r'(.*?)'
-            + r'<td><a href="(.*?)" title="(?P<city_where_is_stadium>.+?)">(.+?)</a></td>'
-            + r'(.*?)'
-            + r'<td>(?P<country_where_is_stadium>.+?)</td>'
-            + r'(.*?)'
-            + r'<td><a href="(.*?)" title="(?P<team_that_trains_at_stadium>.+?)">(.+?)</a>(.*?)</td>'
-            + r'(.*?)'
-            + r'</tr>'
-            )
-
 def page_to_table(page):
     rx = re.compile(r'<tr>'
-                    + r'(.*?)'
-                    + r'<td><a href=(.*?) title=(.*?)>(?P<title_of_stadium>.+?)</a></td>'
-                    + r'(.*?)'
-                    + r'<td>(?P<capacitet_of_stadium>.+?)<sup (.*?)></td>'
-                    + r'(.*?)'
-                    + r'<td><a(.*?)>(?P<city_where_is_stadium>.+?)</a></td>'
-                    + r'(.*?)'
-                    + r'<td>(?P<country_where_is_stadium>.+?)</td>'
-                    + r'(.*?)'
-                    + r'<td><a href=(.*?) title=(.*?)>(?P<team_that_trains_at_stadium>.+?)</a>(.*?)</td>'
-                    + r'(.*?)'
-                    + r'</tr>',                    
+                    r'(.*?)'
+                    r'<td><a href="(\w+)" title="(\w+)">(?P<title_of_stadium>.+?)</a></td>'
+                    r'(.*?)'
+                    r'<td>(?P<capacity_of_stadium>.+?)<sup (.*?)></td>'
+                    r'(.*?)'
+                    r'<td><a(.*?)>(?P<city_where_is_stadium>.+?)</a></td>'
+                    r'(.*?)'
+                    r'<td>(?P<country_where_is_stadium>.+?)</td>'
+                    r'(.*?)'
+                    r'<td><a href="(.*?)" title="(.*?)">(?P<team_that_trains_at_stadium>.+?)</a>(.*?)</td>'
+                    r'(.*?)'
+                    r'</tr>',
                     re.DOTALL)
-    stadiums = re.findall(rx, page)
+    stadiums = re.search(rx, page)
     return stadiums
-                    
+
 table = page_to_table(read_file_to_string(stadiums_directory, frontpage_filename))
 
-
-
-
-
-        
